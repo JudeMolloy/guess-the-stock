e5f0dd7b44cc9850ee476da715be4083fd67d402
Jude Molloy <JudeMolloy07@hotmail.com>
2021-02-25 01:49:49 +0000

    Name form added. Logic is coming along well. Timer implemented.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,26 @@
-from app import app
+from flask import render_template, url_for, session
+from werkzeug.utils import redirect
 
-@app.route('/')
+from app import app
+from app.forms import NameForm
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Hello, World!"
+    form = NameForm()
+    if form.validate_on_submit():
+        # Create new db entry for current game session with name. Or can do this at the end.
+        session['name'] = form.name.data
+        session['score'] = 0
+        return redirect(url_for("play"))
+    return render_template("enter-name.html", form=form)
+
+
+@app.route('/play')
+def play():
+    return render_template("play.html")
+
+@app.route('/game-over')
+def game_over():
+    return render_template("game-over.html")
+nano
