403fb76791e2e2851c77caa2934bc8d989544495
Jude Molloy <JudeMolloy07@hotmail.com>
2021-02-25 02:15:22 +0000

    fixed api key thing

--- app/request.py
+++ app/request.py
@@ -1,8 +1,11 @@
+import os
 import requests
+
+MARKET_STACK_API_KEY = os.getenv('MARKET_STACK_API_KEY')
 
 stocks = ["AAPL", "BB", "GME", "UBER", "TSLA", "SHOP", "PLTR"]
 for name in stocks:
-    object = requests.get(f"http://api.marketstack.com/v1/eod?access_key=713d8bdd9b293056ed674b25b19aeb94&symbols={name}&date_from=2015-01-01").json()
+    object = requests.get(f"http://api.marketstack.com/v1/eod?access_key={MARKET_STACK_API_KEY}&symbols={name}&date_from=2015-01-01").json()
     #if not in db
     #send to db
 
