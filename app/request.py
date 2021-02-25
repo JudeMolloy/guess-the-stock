8c823cbff4ae552c30f1833a139aae4e1589e218
00MB <s2061204@ed.ac.uk>
2021-02-25 04:03:20 +0000

    todo

--- app/request.py
+++ app/request.py
@@ -6,7 +6,7 @@ MARKET_STACK_API_KEY = os.getenv('MARKET_STACK_API_KEY')
 stocks = ["AAPL", "BB", "GME", "UBER", "TSLA", "SHOP", "PLTR"]
 
 price_dict = {}
-for name in stocks:
+for name in stocks: # TODO: replace api key back to var
     object = requests.get(f"http://api.marketstack.com/v1/eod?access_key=713d8bdd9b293056ed674b25b19aeb94&symbols={name}&date_from=2015-01-01").json()
     temp = []
     for data in object["data"]:
