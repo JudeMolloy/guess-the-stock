11fa63aabe5462c4542d425739cabb7316547103
00MB <s2061204@ed.ac.uk>
2021-02-27 22:54:24 +0000

    React setup

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from app import db
+
+
+class Leaderboard(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    score = db.Column(db.Integer)
+
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def __repr__(self):
+        return '<Leaderboard Entry: {} scored {}>'.format(self.name, self.score)
---
+++ app/routes.py
@@ -0,0 +1,38 @@
+from flask import render_template, url_for, session, request
+from werkzeug.utils import redirect
+
+from app import app
+from app.forms import NameForm
+from app.models import Leaderboard
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        # Create new db entry for current game session with name. Or can do this at the end.
+        session['name'] = form.name.data
+        session['score'] = 0
+        return redirect(url_for("play"))
+    return render_template("enter-name.html", form=form)
+
+
+
+@app.route('/play', methods=['GET', 'POST'])
+def play():
+    #if request.method == "POST":
+
+    #else:
+        # answer =
+        # session['answer'] = answer
+    return render_template("play.html")
+
+
+@app.route('/game-over')
+def game_over():
+    # Save score THEN reset THEN render page.
+    score = session['score']
+    name = session['name']
+    leaderboard_entry = Leaderboard(name=name, score=score)
+    session['score'] = 0
+    return render_template("game-over.html")
