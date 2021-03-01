14484cbf55a51cc056283831ef598af69f3d860d
00MB <s2061204@ed.ac.uk>
2021-03-01 22:44:17 +0000

    Script

---
+++ guess.py
@@ -0,0 +1,31 @@
+import os
+import requests
+
+MARKET_STACK_API_KEY = os.getenv('MARKET_STACK_API_KEY')
+
+stocks = ["AAPL", "BB", "GME", "UBER", "TSLA", "SHOP", "PLTR"]
+
+price_dict = {}
+for name in stocks:
+    price_dict = {}
+    object = requests.get(f"http://api.marketstack.com/v1/eod?access_key={MARKET_STACK_API_KEY}&symbols={name}&date_from=2015-01-01").json()
+    object = requests.get(f"http://api.marketstack.com/v1/eod?access_key=713d8bdd9b293056ed674b25b19aeb94&symbols={name}&date_from=2015-01-01").json()
+    temp = []
+    for data in object["data"]:
+        temp.append({"date" : data["date"], "price" : data["close"]})
+    price_dict= {
+        "name": object["data"][0]["symbol"],
+        "data": temp
+    }
+    #if not in db
+    #send to db
+
+# Dictionary format:
+# price_dict = {
+#   "name": "TSLA",
+#   "data": [
+#       {"date": "2020-10-06T00:00:00+0000", "price": 420},
+#       {"date": "2020-10-07T00:00:00+0000", "price": 500},
+#       ...
+#   ]
+# }
