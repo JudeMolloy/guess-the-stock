691ee1888799a6dde96af109f44638a67882e190
Jude Molloy <JudeMolloy07@hotmail.com>
2021-03-02 02:50:38 +0000

    Initialised db. Added some more logic and updated template to make use of data from route.

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import render_template, url_for, session, request
 from werkzeug.utils import redirect
 
@@ -5,6 +7,7 @@ from app import app
 from app.forms import NameForm
 from app.models import Leaderboard, StockData
 from sqlalchemy import func
+from guess import Data
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,13 +24,17 @@ def index():
 
 @app.route('/play', methods=['GET', 'POST'])
 def play():
-    stock = StockData.query.order_by(func.random()).first()
+    stock = StockData.query.order_by(func.random()).first_or_404()
+    answer = stock.name
+    stocks = [x for x in Data.stocks if x != answer]
+    options = random.shuffle(random.sample(stocks, 3).append(answer))
     #if request.method == "POST":
 
     #else:
         # answer =
         # session['answer'] = answer
-    return render_template("play.html", stock_json_string=stock.json_string)
+    return render_template("play.html", stock_json_string=stock.json_string,
+                           a=options[0], b=options[1], c=options[2], d=options[3])
 
 
 @app.route('/game-over')
--- guess.py
+++ guess.py
@@ -3,10 +3,11 @@ import requests
 
 MARKET_STACK_API_KEY = os.getenv('MARKET_STACK_API_KEY')
 
-stocks = ["AAPL", "BB", "GME", "UBER", "TSLA", "SHOP", "PLTR"]
+class Data():
+    stocks = ["AAPL", "BB", "GME", "UBER", "TSLA", "SHOP", "PLTR"]
 
 price_dict = {}
-for name in stocks:
+for name in Data.stocks:
     price_dict = {}
     object = requests.get(f"http://api.marketstack.com/v1/eod?access_key={MARKET_STACK_API_KEY}&symbols={name}&date_from=2015-01-01").json()
     object = requests.get(f"http://api.marketstack.com/v1/eod?access_key=713d8bdd9b293056ed674b25b19aeb94&symbols={name}&date_from=2015-01-01").json()
