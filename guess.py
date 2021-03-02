c8bb23153071386506944129ddf36c44043256f4
00MB <s2061204@ed.ac.uk>
2021-03-02 01:19:43 +0000

    Updated script

--- guess.py
+++ guess.py
@@ -10,22 +10,23 @@ for name in stocks:
     price_dict = {}
     object = requests.get(f"http://api.marketstack.com/v1/eod?access_key={MARKET_STACK_API_KEY}&symbols={name}&date_from=2015-01-01").json()
     object = requests.get(f"http://api.marketstack.com/v1/eod?access_key=713d8bdd9b293056ed674b25b19aeb94&symbols={name}&date_from=2015-01-01").json()
-    temp = []
+    tempdates, tempdata = [], []
     for data in object["data"]:
-        temp.append({"date" : data["date"], "price" : data["close"]})
+        if data["close"] != None:
+            tempdates.append(data["date"])
+            tempdata.append(data["close"])
     price_dict= {
         "name": object["data"][0]["symbol"],
-        "data": temp
+        "dates": tempdates,
+        "data": tempdata
     }
     #if not in db
     #send to db
-
+    
+print(price_dict)
 # Dictionary format:
 # price_dict = {
 #   "name": "TSLA",
-#   "data": [
-#       {"date": "2020-10-06T00:00:00+0000", "price": 420},
-#       {"date": "2020-10-07T00:00:00+0000", "price": 500},
-#       ...
-#   ]
+#   "dates": [2020-10-06T00:00:00+0000", "2020-10-07T00:00:00+0000"],
+#   "data": [420, 500]
 # }
