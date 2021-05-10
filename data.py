b505adc2296a00107a32e5f46283ece3bd513aec
00MB <s2061204@ed.ac.uk>
2021-05-10 18:46:04 +0100

    Working script

--- data.py
+++ data.py
@@ -2,10 +2,14 @@ import json
 import os
 import requests
 from app.models import StockData
+from dotenv import load_dotenv
 
-script = False
+load_dotenv()
+
+script = True
 
 if script:
+    
     MARKET_STACK_API_KEY = os.getenv('MARKET_STACK_API_KEY')
 
     class Data():
@@ -15,9 +19,11 @@ if script:
 
     for name in Data.stocks:
         price_dict = {}
-        object = requests.get(f"http://api.marketstack.com/v1/eod?access_key={MARKET_STACK_API_KEY}&symbols={name}&date_from=2015-01-01").json()
+        try:
+            object = requests.get(f"http://api.marketstack.com/v1/eod?access_key={MARKET_STACK_API_KEY}&symbols={name}&date_from=2015-01-01").json()
+        except:
+            print("--error--")
         tempdates, tempdata = [], []
-        print(object)
         if "error" not in object and object["data"] != []:
             for data in object["data"]:
                 if data["close"] != None:
@@ -28,6 +34,7 @@ if script:
                 "dates": tempdates[::-1],
                 "data": tempdata[::-1]
             }
+            print(price_dict["name"])
 
             # Check if the stock is in the db.
             stock = StockData.query.filter_by(name=price_dict.get("name")).first()
@@ -35,7 +42,7 @@ if script:
             if not stock:
                 stock = StockData(name=price_dict.get("name"), json_string=json.dumps(price_dict))
                 stock.save_to_db()
-                
+    print("--SCRIPT COMPLETE--")
 # Dictionary format:
 # price_dict = {
 #   "name": "TSLA",
